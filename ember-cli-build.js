18d6309f0017f7544e764d1b810ab8d26da499a9
Unai Perez <unaipme@gmail.com>
2016-04-13 15:24:16 +0200

    Trying to make it work

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,6 +1,7 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var ExpressApp = require("express");
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
