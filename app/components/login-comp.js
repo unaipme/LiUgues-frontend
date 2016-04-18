d5c89079f80bd6034a57b7c76b19b29ecd72ec88
Unai Perez <unaipme@gmail.com>
2016-04-18 08:02:01 +0200

    Injection prevention added to login

--- app/components/login-comp.js
+++ app/components/login-comp.js
@@ -3,19 +3,42 @@ import Ember from 'ember';
 var loginComp = Ember.Component.extend({
 	loginSuccess: null,
 	isLoading: false,
+	badContent: false,
 	actions: {
+		checkContent(t) {
+			var r = false;
+			var b = ["'", "\\", ";"];
+			for (var i in b) {
+				r |= (t.indexOf(b[i]) !== -1)
+			}
+			this.set("badContent", r);
+		},
 		connectionError() {
-			Ember.$("#errorLogging").hide();
+			Ember.$(".error_msg").hide();
 			Ember.$("#errorConnecting").fadeIn();
 			setTimeout(function() {
 				Ember.$("#errorConnecting").fadeOut();
 			}, 5000);
 		},
 		loginError() {
-			Ember.$("#errorConnecting").hide();
+			Ember.$(".error_msg").hide();
 			Ember.$("#errorLogging").fadeIn();
 			setTimeout(function() {
 				Ember.$("#errorLogging").fadeOut();
+			}, 5000);
+		},
+		formError() {
+			Ember.$(".error_msg").hide();
+			Ember.$("#errorForm").fadeIn();
+			setTimeout(function() {
+				Ember.$("#errorForm").fadeOut();
+			}, 5000);
+		},
+		contentError() {
+			Ember.$(".error_msg").hide();
+			Ember.$("#errorContent").fadeIn();
+			setTimeout(function() {
+				Ember.$("#errorContent").fadeOut();
 			}, 5000);
 		},
 		loginSuccess(token) {
@@ -25,6 +48,22 @@ var loginComp = Ember.Component.extend({
 			this.set("isLoading", true);
 			var u = Ember.$("#username")[0].value;
 			var p = Ember.$("#password")[0].value;
+			if (u === '' || p === '') {
+				console.log("Form error");
+				this.set("isLoading", false);
+				this.send("formError");
+				return;
+			}
+			this.send("checkContent", u);
+			var c1 = this.get("badContent");
+			this.send("checkContent", p);
+			var c2 = this.get("badContent");
+			if (c1 || c2) {
+				console.log("Content error");
+				this.set("isLoading", false);
+				this.send("contentError");
+				return;
+			}
 			var t = this;
 			Ember.$.ajax("https://liugues-api.herokuapp.com/p/login", {
 				method: "POST",
